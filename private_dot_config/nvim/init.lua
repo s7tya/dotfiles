17d12c8457ba5b09a68ebfd2a5448ae408c1dc41
s7tya <53410646+s7tya@users.noreply.github.com>
2024-02-27 02:56:32 +0900

    configure nvim-cmp

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -63,10 +63,40 @@ require("lazy").setup({
     -- nvim-cmp
     {
       "hrsh7th/nvim-cmp",
+      dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-emoji",
+        "hrsh7th/cmp-buffer",
+        "L3MON4D3/LuaSnip",
+      },
       event = "InsertEnter",
+      config = function()
+        local cmp = require("cmp")
+        cmp.setup({
+          snippet = {
+            expand = function(args)
+              require("luasnip").lsp_expand(args.body)
+            end,
+          },
+          mapping = cmp.mapping.preset.insert({
+            ["<C-p>"] = cmp.mapping.select_prev_item(),
+            ["<C-n>"] = cmp.mapping.select_next_item(),
+            ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+            ["<C-f>"] = cmp.mapping.scroll_docs(4),
+            ["<C-Space>"] = cmp.mapping.complete(),
+            ["<C-e>"] = cmp.mapping.close(),
+            ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          }),
+          sources = cmp.config.sources({
+            { name = "nvim_lsp" },
+            { name = "luasnip" },
+            { name = "emoji" },
+          }, {
+            { name = "buffer" },
+          })
+        })
+      end
     },
-    { "hrsh7th/cmp-nvim-lsp", event = "InsertEnter" },
-    { "hrsh7th/cmp-emoji", event = "InsertEnter" },
 
     -- mini.nvim
     {
@@ -146,6 +176,10 @@ lspconfig.rust_analyzer.setup({
         }
     }
 })
+
+-- nvim-cmp
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
