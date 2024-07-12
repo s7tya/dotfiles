a3fb0535dc8b22e6e9cc58cba6eb929c988b30e6
Shina <53410646+s7tya@users.noreply.github.com>
2024-07-12 10:24:18 +0900

    update

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     # cargo
-    export PATH="$HOME/.cargo/bin:$PATH"
+    # export PATH="$HOME/.cargo/bin:$PATH"
+    export EDITOR="/opt/homebrew/bin/nvim"
 
     # brew
     eval "$(/opt/homebrew/bin/brew shellenv)"
@@ -15,5 +16,10 @@ if status is-interactive
     alias vi="nvim"
     alias cat="bat"
     alias ls="eza --icons --group-directories-first --color=automatic"
+
+    alias ghqcd='cd $(ghq list -p | fzf)'
+
+    alias lualatex="docker run --rm -v $(pwd):/document latex-compiler lualatex"
+    alias latexmk="docker run --rm -v $(pwd):/document latex-compiler latexmk"
 
 end
