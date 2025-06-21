6fe370ade0387b2cc10a240965217fe5be93d94f
shina <53410646+s7tya@users.noreply.github.com>
2025-06-21 11:13:01 +0900

    nvim のコメントアウトされた設定を削除

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -28,14 +28,14 @@ vim.opt.termguicolors = true
 -- plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -55,26 +55,17 @@ vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
 -- plugins:nvim-tree
 vim.api.nvim_set_keymap("n", "<space>t", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
 
--- vim.api.nvim_create_autocmd("BufEnter", {
---   nested = true,
---   callback = function()
---     if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
---       vim.cmd "quit"
---     end
---   end
--- })
-
 -- plugins:conform.nvim
 require("conform").setup({
-	formatters_by_ft = {
-		lua = { "stylua" },
-		rust = { "rustfmt", lsp_format = "fallback" },
-		javascript = { "prettierd", "prettier", stop_after_first = true },
-	},
-	format_on_save = {
-		timeout_ms = 500,
-		lsp_format = "fallback",
-	},
+  formatters_by_ft = {
+    lua = { "stylua" },
+    rust = { "rustfmt", lsp_format = "fallback" },
+    javascript = { "prettierd", "prettier", stop_after_first = true },
+  },
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_format = "fallback",
+  },
 })
 
 -- rust.vim
--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -1,92 +1,92 @@
 return {
-	-- iceberg
-	{ "cocopon/iceberg.vim" },
+  -- iceberg
+  { "cocopon/iceberg.vim" },
 
-	-- lspconfig
-	{ "neovim/nvim-lspconfig" },
+  -- lspconfig
+  { "neovim/nvim-lspconfig" },
 
-	-- nvim-tree.lua
-	{
-		"nvim-tree/nvim-tree.lua",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		opts = {},
-	},
+  -- nvim-tree.lua
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {},
+  },
 
-	-- nvim-cmp
-	{
-		"hrsh7th/nvim-cmp",
-		version = false,
-		dependencies = {
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-emoji",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
-			"L3MON4D3/LuaSnip",
-		},
-		event = "InsertEnter",
-		config = function()
-			local cmp = require("cmp")
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-p>"] = cmp.mapping.select_prev_item(),
-					["<C-n>"] = cmp.mapping.select_next_item(),
-					["<C-d>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.close(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-					{ name = "emoji" },
-				}, {
-					{ name = "buffer" },
-				}),
-			})
-		end,
-	},
+  -- nvim-cmp
+  {
+    "hrsh7th/nvim-cmp",
+    version = false,
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-emoji",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "L3MON4D3/LuaSnip",
+    },
+    event = "InsertEnter",
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.close(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "emoji" },
+        }, {
+          { name = "buffer" },
+        }),
+      })
+    end,
+  },
 
-	-- nvim-tree-sitter
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-	},
+  -- nvim-tree-sitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+  },
 
-	{
-		"stevearc/conform.nvim",
-	},
+  {
+    "stevearc/conform.nvim",
+  },
 
-	-- trouble.nvim
-	{
-		"folke/trouble.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		opts = {},
-	},
+  -- trouble.nvim
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {},
+  },
 
-	-- mini.nvim
-	{
-		"echasnovski/mini.nvim",
-		version = "*",
-		opts = {},
-		config = function()
-			vim.api.nvim_create_autocmd({ "InsertEnter" }, {
-				pattern = "*",
-				callback = function()
-					require("mini.comment").setup({})
-					require("mini.pairs").setup({})
-					require("mini.surround").setup({})
-				end,
-			})
-		end,
-		once = true,
-	},
+  -- mini.nvim
+  {
+    "echasnovski/mini.nvim",
+    version = "*",
+    opts = {},
+    config = function()
+      vim.api.nvim_create_autocmd({ "InsertEnter" }, {
+        pattern = "*",
+        callback = function()
+          require("mini.comment").setup({})
+          require("mini.pairs").setup({})
+          require("mini.surround").setup({})
+        end,
+      })
+    end,
+    once = true,
+  },
 
-	-- wakatime
-	{ "wakatime/vim-wakatime", lazy = false },
+  -- wakatime
+  { "wakatime/vim-wakatime", lazy = false },
 }
